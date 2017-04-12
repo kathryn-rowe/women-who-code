672a5c03f2e9bf6bad063e8294bbfe59125c4d6b
Computer123 <Computer123>
2017-04-12 09:32:40 -0700

    Worked on big O practice

---
+++ README.md
@@ -0,0 +1,17 @@
+# Women Who Code Exercises
+
+Directory for exercises I work on during WWC events/meet-ups.
+
+# Tech stack
+
+Probably python
+
+### Setup/Installation
+
+Install requirements to run locally.
+
+Clone repository:
+
+```sh
+$ git clone https://github.com/kathryn-rowe/women-who-code.git
+```
--- april_10_2017/bigO_practice.py
+++ april_10_2017/bigO_practice.py
@@ -5,4 +5,30 @@ def freq_hash(lst):
     count_dict = {}
 
     for item in lst:
-        count_dict[item] = [count_dict, 0] + 1
+        count_dict[item] = count_dict.get(item, 0) + 1
+
+    print count_dict
+
+
+def find_mode(lst):
+    count_dict = {}
+
+    for item in lst:
+        count_dict[item] = count_dict.get(item, 0) + 1
+
+    highest_freq = []
+
+    max_value = 0
+
+    for key, value in count_dict.iteritems():
+        if value > max_value:
+            max_value = value
+
+    for key, value in count_dict.iteritems():
+        if value == max_value:
+            highest_freq.append(key)
+
+    print highest_freq
+
+freq_hash(lst)
+find_mode(lst)
